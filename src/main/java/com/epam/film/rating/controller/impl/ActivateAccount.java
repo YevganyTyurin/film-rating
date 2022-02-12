c8085b149abb8cf85ae7c4678fc7a3593a47a349
Yevgany Tyurin <evgturin@gmail.com>
2022-02-12 11:32:29 +0300

    add logger to ActivateAccount command

--- src/main/java/com/epam/film/rating/controller/impl/ActivateAccount.java
+++ src/main/java/com/epam/film/rating/controller/impl/ActivateAccount.java
@@ -12,9 +12,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ActivateAccount implements Command {
-
+    private static final Logger logger = LogManager.getLogger(com.epam.film.rating.controller.impl.ActivateAccount.class);
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -27,7 +29,7 @@ public class ActivateAccount implements Command {
 
 
         } catch (ServiceException e) {
-//            logger.error("Exception in adding film.", e);
+            logger.error("Exception in activating account.", e);
 //            TODO exception
         }
     }
